cea04a5bcff7f18db3b9db2a1f739bbc24eb385b
unknown <ujjal>
2021-02-17 23:42:59 +0600

    complete music website included few noticable features

--- js/app.js
+++ js/app.js
@@ -1,51 +1,50 @@
+const searchBtn = document.getElementById("searchBtn");
 const inputValue = document.getElementById("inputValue");
-const searchValue = document.getElementById("searchBtn")
 
-//search song
-function getSongList() {
-fetch(`https://api.lyrics.ovh/suggest/${inputValue.value}`)
-    .then(response => response.json())
-    .then(data => songArrayList(data.data))
-    
-}
-
-const songArrayList = data =>{
-    const songsDiv = document.getElementById("songDiv")
-    songsDiv.innerHTML = '';
-    data.forEach(song => {
-        // console.log(song)
-        const musicItem = document.createElement("div");
-        // musicItem.innerHTML = '';
-        musicItem.className = "single-result row align-items-center my-3 p-3";
-        songsDiv.appendChild(musicItem);
-        musicItem.innerHTML = `
+searchBtn.addEventListener('click', function() {
+    fetch(`https://api.lyrics.ovh/suggest/${inputValue.value}`)
+        .then(res => res.json())
+        .then(data => musicList(data.data))
+        .catch(erro => errorMessage("Something Went Wrong! Please Try Again Leter"))
+})
+const musicList = data =>{
+    const songDiv = document.getElementById("songDiv");
+    songDiv.innerHTML = "";
+    data.forEach(music =>{
+        console.log(music)
+        const musicDiv = document.createElement("div");
+        musicDiv.className = "single-result row align-items-center my-3 p-3";
+        songDiv.appendChild(musicDiv);
+        musicDiv.innerHTML = `
             <div class="col-md-9">
-                <h3 class="lyrics-name">${song.title}</h3>
-                    <p class="author lead">${song.album.title}</span></p>
-                </div>
-                <div class="col-md-3 text-md-right text-center">
-                <button onclick="songLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+                <h3 class="lyrics-name">${music.title}</h3>
+                <p class="author lead">Album by <sp0an>${music.album.title}</sp0an></p>
+                <audio controls>
+                    <source src="${music.preview}" type="audio/ogg">
+                  
+                </audio>
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+                <button onclick="getLyrics('${music.artist.name}','${music.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
         `
-    });
+    })
+}  
+
+const getLyrics = async(artist, title) =>{
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        const songLyrics = document.getElementById("songLyrics");
+        songLyrics.innerText = data.lyrics; 
+    }
+    catch(error){
+        errorMessage("Something Went Wrong! Please Try Again Leter")
+    }
 }
-// https://api.lyrics.ovh/v1/:artist/:title
-const songLyrics = (artist, title)=>{
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-        .then(res => res.json())
-        .then(data => showLyrics(data))
-        .catch(erro => displayError("something went wrong"))
 
+const errorMessage = (error) =>{
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerText = error;
 }
-
-const showLyrics = (data)=>{
-    const lyricsbtn = document.getElementById("songLyrics");
-    lyricsbtn.innerText = data.lyrics; 
-}
-
-
-
-// const displayError = error => {
-//     const errorTag = document.getElementById('error-message');
-//     errorTag.innerText = error;
-// }
